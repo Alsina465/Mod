6d573e3e1709ccae7bacf63d803aee1c2efd78c3
Alsina465 <166528741+Alsina465@users.noreply.github.com>
2024-05-14 16:53:03 -0300

    Update 04-Imagenes.sh

--- 04-Imagenes.sh
+++ 04-Imagenes.sh
@@ -8,11 +8,11 @@ wget -O /home/sistemas/.face   http://10.17.7.47/Mega/alsina/Iconos/face.png
 cd /usr/share/xfce4/backdrops
 rm *.*
 
-wget /usr/share/xfce4/backdrops 10.17.7.47/Mega/alsina/backdrops/016.jpg
-wget /usr/share/xfce4/backdrops 10.17.7.47/Mega/alsina/backdrops/018.jpg
-wget /usr/share/xfce4/backdrops 10.17.7.47/Mega/alsina/backdrops/020.jpg
-wget /usr/share/xfce4/backdrops 10.17.7.47/Mega/alsina/backdrops/022.png
-wget /usr/share/xfce4/backdrops 10.17.7.47/Mega/alsina/backdrops/024.png
+wget /usr/share/xfce4/backdrops 10.17.7.47/Mega/alsina/backdrops/16.jpg
+wget /usr/share/xfce4/backdrops 10.17.7.47/Mega/alsina/backdrops/18.jpg
+wget /usr/share/xfce4/backdrops 10.17.7.47/Mega/alsina/backdrops/20.jpg
+wget /usr/share/xfce4/backdrops 10.17.7.47/Mega/alsina/backdrops/22.png
+wget /usr/share/xfce4/backdrops 10.17.7.47/Mega/alsina/backdrops/24.png
 wget /usr/share/xfce4/backdrops 10.17.7.47/Mega/alsina/backdrops/word.png
 wget /usr/share/xfce4/backdrops 10.17.7.47/Mega/alsina/backdrops/excel.png
 
