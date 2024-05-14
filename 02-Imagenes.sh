95fbe1a56299b427ea312af483cab998e0e16a72
Alsina465 <166528741+Alsina465@users.noreply.github.com>
2024-05-14 13:25:12 -0300

    Create 02-Imagenes.sh

---
+++ 02-Imagenes.sh
@@ -0,0 +1,18 @@
+
+
+#cambia la foto de perfil
+wget -O /home/sistemas/.face   http://10.17.7.47/Mega/alsina/Iconos/face.png
+#cambia la foto del boton
+
+
+#cd  /usr/share/xfce4/backdrops
+cd /usr/share/xfce4/backdrops
+rm *.*
+
+wget /usr/share/xfce4/backdrops 10.17.7.47/Mega/alsina/backdrops/016.jpg
+wget /usr/share/xfce4/backdrops 10.17.7.47/Mega/alsina/backdrops/018.jpg
+wget /usr/share/xfce4/backdrops 10.17.7.47/Mega/alsina/backdrops/020.jpg
+wget /usr/share/xfce4/backdrops 10.17.7.47/Mega/alsina/backdrops/022.jpg
+wget /usr/share/xfce4/backdrops 10.17.7.47/Mega/alsina/backdrops/024.jpg
+
+
