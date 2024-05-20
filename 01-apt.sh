9e64026d886097c9c70f1215a54587ab1ced186f
Alsina465 <166528741+Alsina465@users.noreply.github.com>
2024-05-20 11:40:56 -0300

    Update 01-apt.sh

--- 01-apt.sh
+++ 01-apt.sh
@@ -1,4 +1,4 @@
-sudo apt-get   install -y  git cheese vlc gimp smbclient samba openssh-server okular kazam nautilus net-tools neofetch  conky-all notify-osd libnotify-bin dialog 
+sudo apt-get   install -y  git snmp cheese vlc gimp smbclient samba openssh-server okular kazam nautilus net-tools neofetch  conky-all notify-osd libnotify-bin dialog 
 
 
 
